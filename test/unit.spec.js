05fc933208b87d0e4e317e30637fea4651f863a9
Yuri Tkachenko <yuri@ya.ru>
2016-07-11 15:23:40 +0300

    move to webpack

--- test/unit.spec.js
+++ test/unit.spec.js
@@ -1,3 +1,5 @@
+var ReactiveElements = require('../dist/reactive-elements')
+
 describe("utils", function() {
     window.items = {
         'city': 'Amsterdam',
@@ -6,29 +8,29 @@ describe("utils", function() {
 
     describe(".parseAttributeValue", function(){
         it("should map 5 attribute to 5", function() {
-            expect(window.ReactiveElements.utils.parseAttributeValue('5')).toEqual('5');
+            expect(ReactiveElements.utils.parseAttributeValue('5')).toEqual('5');
         });
 
         it("should convert {window.items} attribute to pointer", function() {
-            expect(window.ReactiveElements.utils.parseAttributeValue('{window.items}')).toEqual(window.items);
+            expect(ReactiveElements.utils.parseAttributeValue('{window.items}')).toEqual(window.items);
         });
 
         it("should convert JSON object literal string to JSON object", function(){
             var jsonSerialized = "{{\"status\": \"active\", \"language\": \"javascript\"}}",
-                jsonDeserialized = window.ReactiveElements.utils.parseAttributeValue(jsonSerialized);
+                jsonDeserialized = ReactiveElements.utils.parseAttributeValue(jsonSerialized);
             expect(jsonDeserialized.status).toEqual('active');
             expect(jsonDeserialized.language).toEqual('javascript');
         });
 
         it("should convert JSON array literal string to JSON object", function(){
             var jsonSerialized = "{[\"language\", \"javascript\"]}",
-                jsonDeserialized = window.ReactiveElements.utils.parseAttributeValue(jsonSerialized);
+                jsonDeserialized = ReactiveElements.utils.parseAttributeValue(jsonSerialized);
             expect(jsonDeserialized[0]).toEqual('language');
             expect(jsonDeserialized[1]).toEqual('javascript');
         });
         it("should not replace single quotes in json", function(){
             var jsonSerialized = "{{\"status\": \"I'm active\", \"language\": \"I'm javascript\"}}",
-                jsonDeserialized = window.ReactiveElements.utils.parseAttributeValue(jsonSerialized);
+                jsonDeserialized = ReactiveElements.utils.parseAttributeValue(jsonSerialized);
             expect(jsonDeserialized.status).toEqual("I'm active");
             expect(jsonDeserialized.language).toEqual("I'm javascript");
         });
@@ -39,26 +41,26 @@ describe("utils", function() {
 
         ATTRIBUTE_NAME_DELIMITERS.forEach(function(delimiter) {
             it("should convert general attributes to React properties", function(){
-                expect(window.ReactiveElements.utils.attributeNameToPropertyName('attribute' + delimiter + 'test')).toEqual('attributeTest');
-                expect(window.ReactiveElements.utils.attributeNameToPropertyName('attribute' + delimiter + 'test' + delimiter + 'long')).toEqual('attributeTestLong');
+                expect(ReactiveElements.utils.attributeNameToPropertyName('attribute' + delimiter + 'test')).toEqual('attributeTest');
+                expect(ReactiveElements.utils.attributeNameToPropertyName('attribute' + delimiter + 'test' + delimiter + 'long')).toEqual('attributeTestLong');
             });
 
             it("should convert attributes prefixed with 'x" + delimiter + "' to React properties", function(){
-                expect(window.ReactiveElements.utils.attributeNameToPropertyName('x' + delimiter + 'attribute')).toEqual('attribute');
-                expect(window.ReactiveElements.utils.attributeNameToPropertyName('x' + delimiter + 'attribute' + delimiter + 'test')).toEqual('attributeTest');
+                expect(ReactiveElements.utils.attributeNameToPropertyName('x' + delimiter + 'attribute')).toEqual('attribute');
+                expect(ReactiveElements.utils.attributeNameToPropertyName('x' + delimiter + 'attribute' + delimiter + 'test')).toEqual('attributeTest');
             });
 
             it("should convert attributes prefixed with 'data" + delimiter + "' to React properties", function(){
-                expect(window.ReactiveElements.utils.attributeNameToPropertyName('data' + delimiter + 'attribute')).toEqual('attribute');
-                expect(window.ReactiveElements.utils.attributeNameToPropertyName('data' + delimiter + 'attribute' + delimiter + 'test')).toEqual('attributeTest');
+                expect(ReactiveElements.utils.attributeNameToPropertyName('data' + delimiter + 'attribute')).toEqual('attribute');
+                expect(ReactiveElements.utils.attributeNameToPropertyName('data' + delimiter + 'attribute' + delimiter + 'test')).toEqual('attributeTest');
             });
 
             it("should convert attributes containing 'x" + delimiter + "' not as a prefix to React properties", function(){
-                expect(window.ReactiveElements.utils.attributeNameToPropertyName('max' + delimiter + 'attribute')).toEqual('maxAttribute');
+                expect(ReactiveElements.utils.attributeNameToPropertyName('max' + delimiter + 'attribute')).toEqual('maxAttribute');
             });
 
             it("should convert attributes containing 'data" + delimiter + "' not as a prefix to React properties", function(){
-                expect(window.ReactiveElements.utils.attributeNameToPropertyName('attribute' + delimiter + 'data' + delimiter + 'test')).toEqual('attributeDataTest');
+                expect(ReactiveElements.utils.attributeNameToPropertyName('attribute' + delimiter + 'data' + delimiter + 'test')).toEqual('attributeDataTest');
             });
         });
     });
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path')
+var webpack = require('webpack')
+var minimize = process.argv.indexOf('-p') !== -1
+
+module.exports = exports = {
+  entry: './index.js',
+  output: {
+    path: './dist',
+    libraryTarget: minimize ? undefined : 'commonjs',
+    filename: 'reactive-elements.' + (minimize ? 'min.' : '') + 'js'
+  },
+  externals: {
+  	react: true,
+  	'react-dom': true
+  },
+  module: {
+  },
+  devtool: '#inline-source-map'
+}
