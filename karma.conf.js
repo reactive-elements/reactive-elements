0a882ffe1fa700cbdf4a37455d4eb49d015af407
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-05-04 14:21:02 -0700

    ci: add travis ci for continuous integration testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,75 +1,68 @@
 // Karma configuration
 // Generated on Wed Feb 11 2015 12:13:53 GMT+0100 (CET)
 module.exports = function(config) {
-  config.set({
+    config.set({
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: "",
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ["jasmine"],
 
+        // list of files / patterns to load in the browser
+        files: ["test/unit.spec.js"],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // list of files to exclude
+        exclude: [],
 
+        plugins: [
+            require("karma-webpack"),
+            "karma-sourcemap-loader",
+            "karma-chrome-launcher",
+            "karma-jasmine"
+        ],
 
-    // list of files / patterns to load in the browser
-    files: [
-        'test/unit.spec.js'
-    ],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            "test/unit.spec.js": ["webpack", "sourcemap"]
+        },
 
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ["progress"],
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // web server port
+        port: 9876,
 
-    plugins: [
- 		require('karma-webpack'),
- 		'karma-sourcemap-loader',
-		'karma-chrome-launcher',
-		'karma-jasmine'
-    ],
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-        'test/unit.spec.js': ['webpack', 'sourcemap']
-    },
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ["Chrome"],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // https://docs.travis-ci.com/user/chrome#Sandboxing
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: "ChromeHeadless",
+                flags: ["--no-sandbox"]
+            }
+        },
 
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-    webpackMiddleware: {
-        noInfo: true
-    }
-  });
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
+        webpackMiddleware: {
+            noInfo: true
+        }
+    });
 };
