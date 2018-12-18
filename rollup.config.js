f105e32a8de0d461982a5f79782bca4daf82f556
Jack Franklin <jack@jackfranklin.net>
2018-12-18 09:20:27 +0000

    Fix output.globals + exposing of utils

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,10 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+};
 export default {
   input: 'src/reactive-elements.js',
   external: ['react', 'react-dom'],
@@ -11,17 +15,20 @@ export default {
       format: 'cjs',
       sourcemap: true,
       file: pkg.main,
+      globals,
     },
     {
       format: 'esm',
       sourcemap: true,
       file: pkg.module,
+      globals,
     },
     {
       format: 'iife',
       name: 'ReactiveElements',
       sourcemap: true,
       file: 'dist/reactive-elements.iife.js',
+      globals,
     },
   ],
   plugins: [resolve(), commonjs(), babel()],
